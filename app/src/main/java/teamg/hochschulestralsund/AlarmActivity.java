dd2721163801e4ce666f8b9a0a89898f9e2ad7a7
Paul Angus Schindler <paulangusschindler@gmail.com>
2018-06-18 22:41:49 +0200

    Notifikation wird beim Alarm erzeugt.

--- app/src/main/java/teamg/hochschulestralsund/AlarmActivity.java
+++ app/src/main/java/teamg/hochschulestralsund/AlarmActivity.java
@@ -1,7 +1,11 @@
 package teamg.hochschulestralsund;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -22,6 +26,7 @@ public class AlarmActivity extends AppCompatActivity {
             setTitle(alarmText);
             TextView textView = findViewById(R.id.textView_alarm_alarmText);
             textView.setText(alarmText);
+            createNotification(alarmText);
         }
 
         player = MediaPlayer.create(getApplicationContext(), R.raw.nice_wake_up_call);
@@ -29,10 +34,31 @@ public class AlarmActivity extends AppCompatActivity {
         player.setLooping(true);
 
         player.start();
+
     }
 
     public void onStopAlarmBtnClicked(View view) {
         player.stop();
         finish();
+    }
+
+    private void createNotification(String alarmText) {
+        String notificationsText = "Erinnerung: " + alarmText;
+
+        Notification.Builder builder = new Notification.Builder(getApplicationContext());
+
+        builder.setContentTitle("Terminerinnerung")
+                .setContentText(notificationsText)
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        builder.setVisibility(Notification.VISIBILITY_PUBLIC);
+
+        Notification notification = builder.build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+
     }
 }
